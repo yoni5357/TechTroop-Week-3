576878abd64ab677dc0aa5b656bd8131ecf66554
Yonatan Gorovetz <goro5357@gmail.com>
2025-07-27 14:28:51 +0300

    promise wrapped func example

--- classExamples/Promises/diceRoll.js
+++ classExamples/Promises/diceRoll.js
@@ -28,3 +28,33 @@ function rollDice(){
 }
 
 rollDice().then(data => console.log(data)).catch(err => console.log(err));
+
+function oldAsyncFunction(callback) {
+    setTimeout(() => {
+        if (Math.random() > 0.5) {
+             callback(null, "Success");
+         } else {
+             callback(new Error("Failed"));
+         }
+     }, 1000);
+}
+
+function promiseWrappedFunction() {
+  return new Promise((resolve, reject) => {
+    oldAsyncFunction((err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+}
+
+promiseWrappedFunction()
+  .then(result => {
+    console.log("Result:", result);
+  })
+  .catch(error => {
+    console.error("Error:", error.message);
+  });
